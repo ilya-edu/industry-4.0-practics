2da8fe6f64a8229093def6eb4f09975b8869e670
ilya-edu <edu.kovalev@gmail.com>
2024-11-10 02:33:40 +0300

    Initial commit

---
+++ GetAndJsonCMake/src/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+#include <cpr/cpr.h>
+#include "../include/json.hpp"
+
+using json = nlohmann::json;
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    
+    cpr::Response r = cpr::Get(cpr::Url{ "https://kav-api.kovalev.team/servodrive/lastActualData?servoDriveId=1" });
+
+    // Статус код
+    std::cout << "Status code: " << r.status_code << '\n';
+    
+    // Заголовки, если нужны
+    //std::cout << "Header:\n";
+    //for (const std::pair<const std::basic_string<char>, std::basic_string<char>>& kv : r.header) {
+    //    std::cout << '\t' << kv.first << ':' << kv.second << '\n';
+    //}
+    
+    // Парсим JSON из запроса и выводим
+    json res_text = json::parse(r.text);
+    /// Перевод из cp1251 в utf8 оставлю самим делать
+    /// Запрос с таймаутом заворачивается легко в цикл
+    //std::cout << res_text["data"][0][1][0][1].get<std::string>() << std::endl;
+    //std::cout << res_text[0]["actPhaseU"].get<std::string>() << std::endl;
+
+    for (auto& [key, value] : res_text[0].items())
+    {
+      std::cout << key << ": " << value << std::endl;
+    }
+
+
+	system("pause>nul");
+	return 0;
+}
---
+++ PythonGetJsonKav/pyKav.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import requests
+import time
+
+url = "https://kav-api.kovalev.team/servodrive/lastActualData?servoDriveId=1"
+
+#payload = {}
+headers = {'Accept': 'application/json'}
+
+
+dataJson = {
+    'actVelocity': [],
+    'actPhaseU': [],
+    'actPhaseV': [],
+    'actOutCurrent': []
+}
+
+x_values = []
+y_values = dataJson['actVelocity']
+y_values2 = dataJson['actPhaseU']
+y_values3 = dataJson['actPhaseV']
+y_values4 = dataJson['actOutCurrent']
+colors = ['blue', 'red', 'orange']
+cols = 2
+rows = 2
+
+
+figure, axis = plt.subplots(rows, cols)
+
+
+def animate(i):
+    response = (requests.request(
+        "GET", url, headers=headers)).json()
+    x_values.append(time.time() - time_start)
+    actVelocity = float((response[0]['actVelocity']).replace(',', '.'))
+    actPhaseU = float((response[0]['actPhaseU']).replace(',', '.'))
+    actPhaseV = float((response[0]['actPhaseV']).replace(',', '.'))
+    actOutCurrent = float((response[0]['actOutCurrent']).replace(',', '.'))
+    dataJson['actVelocity'].append(actVelocity)
+    dataJson['actPhaseU'].append(actPhaseU)
+    dataJson['actPhaseV'].append(actPhaseV)
+    dataJson['actOutCurrent'].append(actOutCurrent)
+    for row in range(rows):
+        for col in range(cols):
+            axis[row, col].cla()
+            axis[row, col].autoscale(enable=True, axis='both', tight=None)
+            axis[row, col].set_title(
+                f"{list(dataJson.keys())[row * cols + col]}")
+    axis[0, 0].plot(x_values, y_values, c=colors[0] if actVelocity <
+                    20 else colors[1])
+    axis[0, 1].plot(x_values, y_values2, c=colors[0] if actPhaseU <
+                    1 else colors[2])
+    axis[1, 0].plot(x_values, y_values3, c=colors[0] if actPhaseV <
+                    2 else colors[1])
+    axis[1, 1].plot(x_values, y_values4, c=colors[0] if actOutCurrent <
+                    2 else colors[1])
+
+
+time_start = time.time()
+ani = animation.FuncAnimation(plt.gcf(), animate, interval=100)
+
+plt.tight_layout()
+plt.show()
